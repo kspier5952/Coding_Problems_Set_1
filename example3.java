a84388cab1242373c9c8e191ed70518fb8ab7a88
kspier5952 <159089040+kspier5952@users.noreply.github.com>
2024-02-08 14:51:33 -0500

    I did the three challenges

--- example3.java
+++ example3.java
@@ -1,3 +1,7 @@
-public class example3 {
-    
+public class example3 
+{
+    public static void main (String[] args)
+    {
+        System.out.println ("Katelyn Spier \n\n Button: Be happy \n Proverb: You are braver than you believe, stronger than you seem, and smarter than you think - A.A Milne");
+    }
 }
